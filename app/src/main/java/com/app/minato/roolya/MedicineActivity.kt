1870d95c6f3be089a3ece8e0521f23fe3b79a1a8
sirirak <hotmail22866@gmail.com>
2018-03-25 22:09:25 +0700

    update

---
+++ app/src/main/java/com/app/minato/roolya/MedicineActivity.kt
@@ -0,0 +1,12 @@
+package com.app.minato.roolya
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medicine)
+    }
+}
